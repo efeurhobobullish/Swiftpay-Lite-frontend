078fde692dad1079fa36290fe67a08e4a6955366
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-19 09:33:52 +0100

    Update SignupPage.jsx

--- src/Pages/Auth/SignupPage.jsx
+++ src/Pages/Auth/SignupPage.jsx
@@ -1,4 +1,4 @@
-import Signup from "../Components/Authentication/Signup"
+import Signup from "../../Components/Authentication/Signup"
 
 const SignupPage = () => {
     document.title = "QuestPay | Create New account"
