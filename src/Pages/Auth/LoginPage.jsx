d7481b1906a3415dc8a981115ebd2f2a5e1bb39d
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-19 09:17:44 +0100

    Create LoginPage.jsx

---
+++ src/Pages/Auth/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Login from "../Components/Authentication/Login"
+
+const LoginPage = () => {
+    document.title = "QuestPay | Login"
+  return (
+      <>
+          <Login/>
+      </>
+  )
+}
+
+export default LoginPage
