728ddc98be0c3f12249811c243b2dea300c47d76
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-19 10:38:23 +0100

    Create Profile.jsx

---
+++ src/Pages/Main/Profile.jsx
@@ -0,0 +1,16 @@
+import Heading from "../../Components/User/Heading"
+import Layout from "../../Components/User/Layout"
+import ProfileCard from "../../Components/User/ProfileCard"
+import SettingsOption from "../../Components/User/SettingsOption"
+
+const Profile = () => {
+  return (
+    <Layout>
+      <Heading title="Profile" />
+      <ProfileCard />
+      <SettingsOption/>
+    </Layout>
+  )
+}
+
+export default Profile
