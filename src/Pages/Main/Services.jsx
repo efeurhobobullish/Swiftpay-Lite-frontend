f45f40d510cb73da34806a7822d840ac172b3e41
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-19 10:40:10 +0100

    Create Services.jsx

---
+++ src/Pages/Main/Services.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import Heading from "../../Components/User/Heading";
+import { services } from "../../constants/data";
+import styles from "../../Styles/services.module.css";
+import Layout from "../../Components/User/Layout";
+const Services = () => {
+  document.title = "QuestPay | Services";
+  return (
+    <>
+      <Layout>
+        <Heading title="Services" />
+
+        <ul className={styles.list}>
+          {services.map((item, index) => {
+            return (
+              <Link className={styles.box} to={item.path} key={index}>
+                <li className={styles.card}>
+                  <div className={styles.icon}>
+                    <span className="material-symbols-outlined">
+                      {item.icon}
+                    </span>
+                  </div>
+                  <div>{item.name}</div>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </Layout>
+    </>
+  );
+};
+
+export default Services;
