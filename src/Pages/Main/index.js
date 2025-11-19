40f48e91d581faed863afb5dc33470550a859116
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-19 10:54:21 +0100

    Create index.js

---
+++ src/Pages/Main/index.js
@@ -0,0 +1,15 @@
+export { default as Dashboard } from "./Dashboard";
+export { default as ChangePin } from "./ChangePin";
+export { default as TransactionDetails } from "./TransactionDetails";
+export { default as Wallet } from "./Wallet";
+export { default as FundWallet } from "./FundWallet";
+export { default as Services } from "./Services";
+export { default as Profile } from "./Profile";
+export { default as Share } from "./Share";
+export { default as UpdateProfile } from "./UpdateProfile";
+export { default as UpdatePassword } from "./UpdateProfile";
+export { default as LoginActivity } from "./LoginActivity";
+export { default as Support } from "./Support";
+export { default as SupportChat } from "./SupportChat";
+export { default as Notifications } from "./Notifications";
+export { default as NotificationDetails } from "./NotificationDetails";
