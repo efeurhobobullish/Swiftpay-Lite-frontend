8f8b223852044b8259c09eb4bac6d80ff90cfe59
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-19 09:19:47 +0100

    Create VerifyPage.jsx

---
+++ src/Pages/Auth/VerifyPage.jsx
@@ -0,0 +1,12 @@
+import Verify from "../Components/Authentication/Verify"
+
+const VerifyPage = () => {
+    document.title = "QuestPay | Verify"
+  return (
+      <>
+          <Verify/>
+      </>
+  )
+}
+
+export default VerifyPage
