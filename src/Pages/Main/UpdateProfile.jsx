ac8b7a874a2439f0c7bfaf32e0ad332fe3276b82
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-19 10:37:07 +0100

    Create UpdateProfile.jsx

---
+++ src/Pages/Main/UpdateProfile.jsx
@@ -0,0 +1,19 @@
+import Heading from "../Components/User/Heading"
+import Layout from "../Components/User/Layout"
+import ProfileForm from "../Components/User/ProfileForm"
+import styles from "../Styles/update.module.css"
+const UpdateProfile = () => {
+    
+  return (
+      <>
+          <Layout>
+              <Heading title="Update Profile" />
+              <div className={styles.container}>
+                  <ProfileForm/>
+              </div>
+      </Layout>
+      </>
+  )
+}
+
+export default UpdateProfile
