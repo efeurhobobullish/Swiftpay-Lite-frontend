93cdbffe11ea80c864027b368850556cd7cf5a9e
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-19 12:38:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,23 @@ import ThemeContextProvider from "./Context/ThemeContextProvider";
 import NotFound from "./Pages/NotFound";
 
 const Onboarding = lazy(() => import("./Pages/Onboarding"));
-const Login = lazy(() => import("./Pages/Auth/Login"));
-const Signup = lazy(() => import("./Pages/Auth/Signup"));
-const Verify = lazy(() => import("./Pages/Auth/Verify"));
+const LoginPage = lazy(() => import("./Pages/Auth/Login"));
+const SignupPage = lazy(() => import("./Pages/Auth/Signup"));
+const VerifyPage = lazy(() => import("./Pages/Auth/Verify"));
 
 const Dashboard = lazy(() => import("./Pages/Main/Dashboard"));
 const Wallet = lazy(() => import("./Pages/Main/Wallet"));
-const FundWallet = lazy(() => import("./Pages/Main/FundWallet"));
-const Profile = lazy(() => import("./Pages/Main/Profile"));
-const UpdateProfile = lazy(() => import("./Pages/Main/UpdateProfile"));
-const UpdatePassword = lazy(() => import("./Pages/Main/UpdatePassword"));
-const ChangePin = lazy(() => import("./Pages/Main/ChangePin"));
+const FundWalletPage = lazy(() => import("./Pages/Main/FundWallet"));
+const ProfilePage = lazy(() => import("./Pages/Main/Profile"));
+const UpdateProfilePage = lazy(() => import("./Pages/Main/UpdateProfile"));
+const UpdatePasswordPage = lazy(() => import("./Pages/Main/UpdatePassword"));
+const ChangePinPage = lazy(() => import("./Pages/Main/ChangePin"));
 
-const Service = lazy(() => import("./Pages/Services/Service"));
-const Airtime = lazy(() => import("./Pages/Services/Airtime"));
-const Data = lazy(() => import("./Pages/Services/Data"));
-const Cable = lazy(() => import("./Pages/Services/Cable"));
-const Power = lazy(() => import("./Pages/Services/Power"));
+const ServicePage = lazy(() => import("./Pages/Services/Service"));
+const AirtimePage = lazy(() => import("./Pages/Services/Airtime"));
+const DataPage = lazy(() => import("./Pages/Services/Data"));
+const CablePage = lazy(() => import("./Pages/Services/Cable"));
+const PowerPage = lazy(() => import("./Pages/Services/Power"));
 
 const App = () => {
   return (
@@ -28,21 +28,21 @@ const App = () => {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Onboarding />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/verify" element={<Verify />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/verify" element={<VerifyPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/wallet" element={<Wallet />} />
-          <Route path="/fund-wallet" element={<FundWallet />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/update-profile" element={<UpdateProfile />} />
-          <Route path="/update-password" element={<UpdatePassword />} />
-          <Route path="/change-pin" element={<ChangePin />} />
-          <Route path="/services" element={<Service />} />
-          <Route path="/airtime" element={<Airtime />} />
-          <Route path="/data" element={<Data />} />
-          <Route path="/cable" element={<Cable />} />
-          <Route path="/electricity" element={<Power />} />
+          <Route path="/fund-wallet" element={<FundWalletPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/update-profile" element={<UpdateProfilePage />} />
+          <Route path="/update-password" element={<UpdatePasswordPage />} />
+          <Route path="/change-pin" element={<ChangePinPage />} />
+          <Route path="/services" element={<ServicePage />} />
+          <Route path="/airtime" element={<AirtimePage />} />
+          <Route path="/data" element={<DataPage />} />
+          <Route path="/cable" element={<CablePage />} />
+          <Route path="/electricity" element={<PowerPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
